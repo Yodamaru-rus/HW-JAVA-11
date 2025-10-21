a501256368f9008bef4d078d4a7ba89b316dad1c
Vitkovskiy <vitkovskiy1912@mail.ru>
2025-10-21 11:57:30 +0300

    Change the implementation without changing count
    Created a separate variable inside the function to determine the size of the final array

--- src/main/java/FilmsManager.java
+++ src/main/java/FilmsManager.java
@@ -24,11 +24,14 @@ public class FilmsManager {
     }
 
     public Films[] findLast() {
+        int reversedLength;
         if (count > catalogFilms.length) {
-            count = catalogFilms.length;
+            reversedLength = catalogFilms.length;
+        } else {
+            reversedLength = count;
         }
-        Films[] reversed = new Films[count];
-        for (int i = 0; i < reversed.length; i++) {
+        Films[] reversed = new Films[reversedLength];
+        for (int i = 0; i < reversedLength; i++) {
             reversed[i] = catalogFilms[catalogFilms.length - 1 - i];
         }
         return reversed;
